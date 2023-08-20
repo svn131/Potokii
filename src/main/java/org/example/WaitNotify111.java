edf7d8c668a088ddb068a0aeb7a640e292bfe2ae
Vladimir Solovyov <svn131131@gmail.com>
2023-08-20 17:54:03 +0500

    9

--- src/main/java/org/example/WaitNotify111.java
+++ src/main/java/org/example/WaitNotify111.java
@@ -5,14 +5,20 @@ public class WaitNotify111 {
 
         long startTime = System.currentTimeMillis();
 
+
+
         int length = 1000;
         Integer[] array = new Integer[length];
 
         Object lock = new Object();
+
         Thread thread1 = new Thread(() -> {
             synchronized (lock) {
+                System.out.println("11111111");
+
+
                 for (int i = 0; i < length / 4; i++) {
-                    if (i % 2 == 0) {
+                    if(i % 2 == 0) {
                         for (int y = 0; y < 10000000; y++) {
                             if (y == 980007) {
                                 array[i] = y;
@@ -20,32 +26,27 @@ public class WaitNotify111 {
                                 array[i] = 8;
                             }
                         }
-                    } else {
-                        for (int y = 0; y < 10000000; y++) {
-                            if (y == 980005) {
-                                array[i] = y;
-                            } else {
-                                array[i] = 8;
+                    }
+                    else {
+                        for (int y = 0 ; y < 10000000 ; y++){
+                            if(y == 980005) {
+                                array[i]= y; }
+                            else {
+                                array[i]= 8;
                             }
                         }
                     }
                 }
+
                 lock.notifyAll();
             }
         });
 
         Thread thread2 = new Thread(() -> {
             synchronized (lock) {
-
-                try {
-
-
-                    lock.wait();
+                System.out.println("2222222222222222222222");
 
 
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
 
                 for (int i = length / 4; i < length / 2; i++) {
                     if (i % 2 == 0) {
@@ -72,11 +73,8 @@ public class WaitNotify111 {
 
         Thread thread3 = new Thread(() -> {
             synchronized (lock) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println("3333333333333333333333");
+
                 for (int i = length / 2; i < length * 3 / 4; i++) {
                     if (i % 2 == 0) {
                         for (int y = 0; y < 10000000; y++) {
@@ -102,11 +100,8 @@ public class WaitNotify111 {
 
         Thread thread4 = new Thread(() -> {
             synchronized (lock) {
-                try {
-                    lock.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println("4444444444");
+
                 for (int i = length * 3 / 4; i < length; i++) {
                     if (i % 2 == 0) {
                         for (int y = 0; y < 10000000; y++) {
@@ -126,24 +121,52 @@ public class WaitNotify111 {
                         }
                     }
                 }
+
+                lock.notifyAll();
             }
         });
 
         thread1.start();
 
         synchronized (lock) {
-            lock.wait();
+            lock.wait(1000000);
         }
 
+
         thread2.start();
+
+        synchronized (lock) {
+            lock.wait(1000000);
+
+
         thread3.start();
+
+
+
+        }
+
+        synchronized (lock) {
+            lock.wait(1000000);
+        }
+
         thread4.start();
+
+//        synchronized (lock) {
+//            lock.wait();
+//        }
+        System.out.println("star234");
+
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        System.out.println("777777777777777777777777777");
 
         thread1.join();
         thread2.join();
         thread3.join();
         thread4.join();
 
+        System.out.println("88888888888888888888888888");
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
 
