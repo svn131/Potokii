088d7056406e033d835c1fea5aafc213203ef9d4
Vladimir Solovyov <svn131131@gmail.com>
2023-08-20 18:13:08 +0500

    11

---
+++ src/main/java/org/example/SostoyanyePotokov.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class SostoyanyePotokov {
+
+
+
+        public static void main(String[] args) throws InterruptedException {
+            Thread thread = new Thread(() -> {
+                System.out.println("Поток запущен");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+            System.out.println("Состояние потока до его запуска: " + thread.getState());
+
+            thread.start();
+
+            System.out.println("Состояние потока после его запуска: " + thread.getState());
+
+            Thread.sleep(500);
+
+            System.out.println("Состояние потока через 500 миллисекунд: " + thread.getState());
+
+            thread.join();
+
+            System.out.println("Состояние потока после его завершения: " + thread.getState());
+        }
+    }
+
+
+
+
