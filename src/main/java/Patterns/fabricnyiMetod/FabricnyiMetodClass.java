e0d62e72c93a49d61f20ee2626cb71f83b1e275d
svn13 <svn131131@gmail.com>
2023-10-06 16:53:04 +0300

    7

--- src/main/java/Patterns/fabricnyiMetod/FabricnyiMetodClass.java
+++ src/main/java/Patterns/fabricnyiMetod/FabricnyiMetodClass.java
@@ -9,8 +9,12 @@ public class FabricnyiMetodClass {
 
         List<Chocolate> chocolateList = new ArrayList<>();
 
+        Factory factory = new Creator(1); // 0 или 1 определяет что будем создавать белый или черный шаколад
+        Chocolate chocolate = factory.create();
+        chocolate.printPrise();
+
 
     }
 }
 
-
+//https://www.youtube.com/watch?v=fC83ITS_NHQ
